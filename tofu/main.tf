41b3502b913c0f170b7a117caea4f42e43ba94d0
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-22 21:30:49 -0400

    Provision *arr VM creation

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_providers {
+    // https://search.opentofu.org/provider/bpg/proxmox/latest
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.111.1"
@@ -7,16 +8,17 @@ terraform {
   }
 }
 
-variable "ssh_key_path" {
-  type      = string
-  sensitive = true
-  default   = "~/.ssh/id_ed25519.pub"
-}
+// Proxmox login credentials
 variable "proxmox_endpoint" { type = string }
 variable "proxmox_username" { type = string }
 variable "proxmox_password" {
   type      = string
   sensitive = true
+}
+variable "ssh_key_path" { // Allow SSH login to all resources using localhost key
+  type      = string
+  sensitive = true
+  default   = "~/.ssh/id_ed25519.pub"
 }
 variable "gateway_ip" { type = string }
 
@@ -27,6 +29,71 @@ provider "proxmox" {
   password = var.proxmox_password
 }
 
+// *arr Ubuntu VM
+resource "proxmox_virtual_environment_vm" "arr_vm" {
+  name        = "arr"
+  description = "Managed by Terraform"
+  tags        = ["terraform", "ubuntu", "arr"]
+
+  node_name = "homelab"
+  vm_id     = 100
+
+  agent {
+    enabled = false
+  }
+
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 2048
+    floating  = 2048
+  }
+
+  scsi_hardware = "virtio-scsi-pci"
+
+  disk {
+    datastore_id = "local-lvm"
+    import_from  = proxmox_download_file.ubuntu_26_resolute_img.id
+    interface    = "scsi0"
+    size         = 20
+  }
+
+  initialization {
+    datastore_id = "local-lvm"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.0.100/24"
+        gateway = var.gateway_ip
+      }
+    }
+
+    user_account {
+      username = "arr"
+      keys     = [trimspace(file(pathexpand(var.ssh_key_path)))]
+    }
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
+
+// Ubuntu Server cloud image
+resource "proxmox_download_file" "ubuntu_26_resolute_img" {
+  content_type       = "import"
+  datastore_id       = "local"
+  node_name          = "homelab"
+  file_name          = "resolute-server-cloudimg-amd64.qcow2"
+  url                = "https://cloud-images.ubuntu.com/resolute/current/resolute-server-cloudimg-amd64.img"
+  checksum           = "9dc7c5363c0146a08ba0c9aa834d82c2c6dfbb1c471ad9a2f0aba1189e21be05"
+  checksum_algorithm = "sha256"
+}
+
+/*
 variable "lxcs" {
   type = map(object({
     vm_id         = number
@@ -92,3 +159,4 @@ resource "proxmox_virtual_environment_container" "containers" {
     type             = "alpine"
   }
 }
+*/
