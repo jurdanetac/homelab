26b1187a09dd6e6d06c3677f416f00233d924f21
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-22 22:18:29 -0400

    Change to Debian image

--- tofu/main.tf
+++ tofu/main.tf
@@ -29,17 +29,21 @@ provider "proxmox" {
   password = var.proxmox_password
 }
 
-// *arr Ubuntu VM
+// *arr Debian VM
 resource "proxmox_virtual_environment_vm" "arr_vm" {
   name        = "arr"
   description = "Managed by Terraform"
-  tags        = ["terraform", "ubuntu", "arr"]
+  tags        = ["terraform", "debian", "arr"]
 
   node_name = "homelab"
   vm_id     = 100
 
   agent {
-    enabled = false
+    enabled = true
+    trim    = true
+    wait_for_ip {
+      disabled = false
+    }
   }
 
   cpu {
@@ -48,7 +52,7 @@ resource "proxmox_virtual_environment_vm" "arr_vm" {
   }
 
   memory {
-    dedicated = 2048
+    dedicated = 6144
     floating  = 2048
   }
 
@@ -56,7 +60,7 @@ resource "proxmox_virtual_environment_vm" "arr_vm" {
 
   disk {
     datastore_id = "local-lvm"
-    import_from  = proxmox_download_file.ubuntu_26_resolute_img.id
+    import_from  = proxmox_download_file.debian_13_trixie_img.id
     interface    = "scsi0"
     size         = 20
   }
@@ -82,15 +86,15 @@ resource "proxmox_virtual_environment_vm" "arr_vm" {
   }
 }
 
-// Ubuntu Server cloud image
-resource "proxmox_download_file" "ubuntu_26_resolute_img" {
+// Debian Server cloud image
+resource "proxmox_download_file" "debian_13_trixie_img" {
   content_type       = "import"
   datastore_id       = "local"
   node_name          = "homelab"
-  file_name          = "resolute-server-cloudimg-amd64.qcow2"
-  url                = "https://cloud-images.ubuntu.com/resolute/current/resolute-server-cloudimg-amd64.img"
-  checksum           = "9dc7c5363c0146a08ba0c9aa834d82c2c6dfbb1c471ad9a2f0aba1189e21be05"
-  checksum_algorithm = "sha256"
+  file_name          = "debian-13-generic-amd64.qcow2"
+  url                = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+  checksum           = "77429b411b39b43f914dc9d14bf34aa315489a1a12b5429f72e5b483bdda23c65698d33443c85d3f3ad7c3a0828ae60845406d6b99646342554d17abae29c2a3"
+  checksum_algorithm = "sha512"
 }
 
 /*
