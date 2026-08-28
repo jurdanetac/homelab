e89e0bf04a70ebded74ea919f91edcfb2eecb052
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-28 18:59:09 -0400

    Disable checksum on debian image since I use latest

--- tofu/images.tf
+++ tofu/images.tf
@@ -1,10 +1,10 @@
 // Debian Server cloud image
 resource "proxmox_download_file" "debian_13_trixie_img" {
-  content_type       = "import"
-  datastore_id       = "local"
-  node_name          = "homelab"
-  file_name          = "debian-13-generic-amd64.qcow2"
-  url                = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
-  checksum           = "77429b411b39b43f914dc9d14bf34aa315489a1a12b5429f72e5b483bdda23c65698d33443c85d3f3ad7c3a0828ae60845406d6b99646342554d17abae29c2a3"
-  checksum_algorithm = "sha512"
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "homelab"
+  file_name    = "debian-13-generic-amd64.qcow2"
+  url          = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+  # checksum           = "184761b0dad0f9ace02f9298050ca96ce3caa39a461a47706d47ff9698b59933918b91b40177fbd4d392f6446af8b4d18ecb94caca988169b19641606bf34003"
+  # checksum_algorithm = "sha512"
 }
